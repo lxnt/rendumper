aea6e8111c403ad70601a6118866dd98172a8fe0
Alexander Sabourenkov <screwdriver@lxnt.info>
2012-12-05 02:32:24 +0400

    insist on hiding everything but the entry points

--- modules/include/ideclspec.h
+++ modules/include/ideclspec.h
@@ -1,14 +1,18 @@
 #if defined(DECLSPEC)
 # undef DECLSPEC
 #endif
-#if defined(__WIN32__)
+#if defined(__WIN32__) || defined(__CYGWIN__)
 # if defined(DFMODULE_BUILD)
 #  define DECLSPEC __declspec(dllexport)
 # else
 #  define DECLSPEC __declspec(dllimport)
 # endif
 # define APIENTRY __stdcall
-#else
-# define DECLSPEC
+#else /* GCC is implied */
+# if defined(DFMODULE_BUILD) /* building modules */
+#  define DECLSPEC __attribute__((visibility ("default")))
+# else /* building glue or linking directly */
+#  define DECLSPEC
+# endif
 # define APIENTRY
 #endif

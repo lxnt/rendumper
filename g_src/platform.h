da3fa0da7726a8877f04967f746205555cad0126
Alexander Sabourenkov <screwdriver@lxnt.info>
2012-12-29 00:13:42 +0400

    fix horrible oversight

--- g_src/platform.h
+++ g_src/platform.h
@@ -1,6 +1,12 @@
 #ifndef _PLATFORM_H_
 #define _PLATFORM_H_
-#include "iplatform.h"
+
+#ifdef _WIN32
+# undef WINDOWS_LEAN_AND_MEAN
+# define WINDOWS_LEAN_AND_MEAN
+# include <windows.h>
+#else
+# include "iplatform.h"
 
 DWORD GetTickCount();	// returns ms since system startup
 BOOL CreateDirectory(const char* pathname, void* unused);
@@ -9,6 +15,7 @@ BOOL QueryPerformanceCounter(LARGE_INTEGER* performanceCount);
 BOOL QueryPerformanceFrequency(LARGE_INTEGER* performanceCount);
 int MessageBox(HWND *dummy, const char* text, const char* caption, UINT type);
 char* itoa(int value, char* result, int base);
+#endif // _WIN32
 
 #endif // _PLATFORM_H_
 

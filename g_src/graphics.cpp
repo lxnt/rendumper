475e0c66a63f637130f38b7a11d7cd9071b61775
Alexander Sabourenkov <screwdriver@lxnt.info>
2013-01-01 23:33:13 +0400

    fix up ttf-related code readability

--- g_src/graphics.cpp
+++ g_src/graphics.cpp
@@ -116,79 +116,74 @@ static void abbreviate_string_helper_hackaroundmissingcode(string &str, int len)
 static void abbreviate_string_hackaroundmissingcode(string &str, int32_t len)
 {
 #if defined(TTF_SUPPORT)
-  if (ttf_manager.ttf_active()) {
-    // We'll need to use TTF-aware text shrinking.
-    while (ttf_manager.size_text(str) > len)
-      abbreviate_string_helper_hackaroundmissingcode(str, str.length() - 1);
-  } else
+    if (ttf_manager.ttf_active()) {
+        // We'll need to use TTF-aware text shrinking.
+        while (ttf_manager.size_text(str) > len)
+            abbreviate_string_helper_hackaroundmissingcode(str, str.length() - 1);
+    } else {
+#else
+    {
 #endif
-  if(str.length()>len) {
-    // 1 letter = 1 tile.
-    abbreviate_string_helper_hackaroundmissingcode(str, len);
-  }
+        if (str.length() > len) {
+            // 1 letter = 1 tile.
+            abbreviate_string_helper_hackaroundmissingcode(str, len);
+        }
+    }
 }
-
 
 void graphicst::addst(const string &str_orig, justification just, int space)
 {
-  if (!str_orig.size())
-    return;
-  string str = str_orig;
-  if (space)
-    abbreviate_string_hackaroundmissingcode(str, space);
-  if (just == not_truetype || true
+    if (!str_orig.size())
+        return;
+
+    string str = str_orig;
+    if (space)
+        abbreviate_string_hackaroundmissingcode(str, space);
+
 #if defined(TTF_SUPPORT)
-        || !ttf_manager.ttf_active()
+    if (just == not_truetype || !ttf_manager.ttf_active()) {
 #endif
-        ) {
-    int s;
-    for(s=0;s<str.length()&&screenx<init.display.grid_x;s++)
-      {
-        if(screenx<0)
-          {
-            s-=screenx;
-            screenx=0;
-            if(s>=str.length())break;
-          }
-        
-        addchar(str[s]);
-      }
-    }
+        for (int s = 0; s < str.length() && screenx < init.display.grid_x ; s++) {
+            if (screenx < 0) {
+                s -= screenx;
+                screenx = 0;
+                if (s >= str.length())
+                    break;
+            }
+            addchar(str[s]);
+        }
 #if defined(TTF_SUPPORT)
-  else {
-    // Truetype
-    if (str.size() > 2 && str[0] == ':' && str[1] == ' ')
-      str[1] = '\t'; // EVIL HACK
-    struct ttf_id id = {str, screenf, screenb, screenbright};
-    ttfstr.push_back(id);
-    // if (str.size() == 80) {
-    //   cout << "(" << int(str[0]) << ") ";
-    // }
-    // cout << screeny << "," << str.size() << ":" << str;
-    // if (just == justify_cont)
-    //   cout << "|";
-    // else
-    //   cout << endl;
-    if (just == justify_cont)
-      return; // More later
-    // This string is done. Time to render.
-    ttf_details details = ttf_manager.get_handle(ttfstr, just);
-    const int handle = details.handle;
-    const int offset = details.offset;
-    int width = details.width;
-    const int ourx = screenx + offset;
-    unsigned int * const s = ((unsigned int*)screen + ourx*dimy + screeny);
-    if (s < (unsigned int*)screen_limit)
-      s[0] = (((unsigned int)GRAPHICSTYPE_TTF) << 24) | handle;
-    // Also set the other tiles this text covers, but don't write past the end.
-    if (width + ourx >= dimx)
-      width = dimx - ourx - 1;
-    for (int x = 1; x < width; ++x)
-      s[x * dimy] = (((unsigned int)GRAPHICSTYPE_TTFCONT) << 24) | handle;
-    // Clean up, prepare for next string.
-    screenx = ourx + width;
-    ttfstr.clear();
-  }
+    } else {
+        // Truetype
+        if (str.size() > 2 && str[0] == ':' && str[1] == ' ')
+            str[1] = '\t'; // EVIL HACK
+        
+        struct ttf_id id = { str, screenf, screenb, screenbright };
+        ttfstr.push_back(id);
+        if (just == justify_cont)
+            return; // More later
+
+        // This string is done. Time to render.
+        ttf_details details = ttf_manager.get_handle(ttfstr, just);
+        const int handle = details.handle;
+        const int offset = details.offset;
+        int width = details.width;
+        const int ourx = screenx + offset;
+        unsigned int * const s = ((unsigned int*)screen + ourx*dimy + screeny);
+        if (s < (unsigned int *) screen_limit)
+            s[0] = (((unsigned int)GRAPHICSTYPE_TTF) << 24) | handle;
+
+        // Also set the other tiles this text covers, but don't write past the end.
+        if (width + ourx >= dimx)
+            width = dimx - ourx - 1;
+
+        for (int x = 1; x < width; ++x)
+            s[x * dimy] = (((unsigned int)GRAPHICSTYPE_TTFCONT) << 24) | handle;
+
+        // Clean up, prepare for next string.
+        screenx = ourx + width;
+        ttfstr.clear();
+    }
 #endif
 }
 
